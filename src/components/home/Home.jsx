c8ad3ab972121e2296cf30d34e64ab6f858dea68
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-14 21:00:51 +0500

    home component added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import Founder from "./Founder";
+import Menu from "./Menu";
 const Home = () => {
-  return <div>Home</div>;
+  const options = {
+    initial: { x: "-100%", opacity: 0 },
+    whileInView: { x: 0, opacity: 1 },
+  };
+  return (
+    <>
+      <section className="home">
+        <div>
+          <motion.h1 {...options}>C̼S̼ B̼u̼r̼g̼e̼r̼ W̼a̼l̼a̼</motion.h1>
+          <motion.p {...options} transition={{ delay: 0.2 }}>
+            Purchase a yummy and tasty burger here.
+          </motion.p>
+        </div>
+        <motion.a
+          href="#menu"
+          initial={{
+            opacity: 0,
+            y: "-100%",
+          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.4 }}>
+          Explore Menu
+        </motion.a>
+      </section>
+      <Founder />
+      <Menu />
+    </>
+  );
 };
 
 export default Home;

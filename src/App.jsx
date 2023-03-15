84717e7da912d6929e4ae58c53959605736610f9
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-15 13:52:00 +0500

    App component chnged

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import Contact from "./components/contact/Contact";
+
 import "./styles/app.scss";
 import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/founder.scss";
+import "./styles/menu.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }

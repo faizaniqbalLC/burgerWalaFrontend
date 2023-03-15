5ba41377e1ef595da35b0e2d21b2e7605ebf8bc6
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-15 20:55:33 +0500

    worked on confirmOrder

---
+++ src/components/cart/ConfirmOrder.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ConfirmOrder = () => {
+  return (
+    <section className="confirmOrder">
+      <main>
+        <h1>Confirm Order</h1>
+        <form>
+          <div>
+            <label>Cash on Delivery</label>
+            <input type="radio" name="payment" />
+          </div>
+          <div>
+            <label>Online</label>
+            <input type="radio" name="payment" />
+          </div>
+          <button type="submit">Place Order</button>
+        </form>
+      </main>
+    </section>
+  );
+};
+
+export default ConfirmOrder;

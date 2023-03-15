d5c1958f613937f060dbb8bc1652c291894878f8
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-15 13:50:03 +0500

    contact form created

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const Contact = () => {
-  return <section className="contact"></section>;
+  return (
+    <section className="contact">
+      <motion.form>
+        <h2>Contact Us</h2>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <textarea cols="30" rows="10" placeholder="Message" />
+        <button type="submit">Send</button>
+      </motion.form>
+      <motion.div className="formBorder">
+        <motion.div></motion.div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Contact;

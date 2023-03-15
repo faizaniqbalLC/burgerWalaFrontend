332101f1a1971ffb92110d1432ae0e05338541fc
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-15 20:57:37 +0500

    worked on paymentsuccess

---
+++ src/components/cart/PaymentSuccess.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PAymentSuccess = () => {
+  return (
+    <section className="paymentSuccess">
+      <main>
+        <h1>Order Confirmed.</h1>
+        <p>Order Placed Successfully, You can check order status below</p>
+        <Link to="/myorders">Check Status</Link>
+      </main>
+    </section>
+  );
+};
+
+export default PAymentSuccess;

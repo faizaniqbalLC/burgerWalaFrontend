c02a5fc1b912aa3367af4d35824f5897cd905183
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-15 16:13:04 +0500

    worked on Contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { motion } from "framer-motion";
+import bur from "../../assets/burger2.png";
 const Contact = () => {
   return (
     <section className="contact">
-      <motion.form>
+      <motion.form
+        initial={{
+          x: "-100vw",
+          opacity: 0,
+        }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.2 }}>
         <h2>Contact Us</h2>
         <input type="text" placeholder="Name" />
         <input type="email" placeholder="Email" />
         <textarea cols="30" rows="10" placeholder="Message" />
         <button type="submit">Send</button>
       </motion.form>
-      <motion.div className="formBorder">
-        <motion.div></motion.div>
+      <motion.div
+        className="formBorder"
+        initial={{
+          x: "100vw",
+          opacity: 0,
+        }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.2 }}>
+        <motion.div
+          initial={{ y: "-100vh", opacity: 0, x: "50%" }}
+          animate={{ y: "-50%", opacity: 1, x: "50%" }}
+          transition={{ delay: 1 }}>
+          <img src={bur} alt="burger" />
+        </motion.div>
       </motion.div>
     </section>
   );

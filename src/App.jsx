1fe2c2fbb36d12d0b52280ad4bd8727ee93113e9
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-16 21:53:16 +0500

    worked on App Component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ import ConfirmOrder from "./components/cart/ConfirmOrder";
 import PaymentSuccess from "./components/cart/PaymentSuccess";
 import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
-import MyOrders from "./components/myOrders/MyOrders.jsx";
+import MyOrders from "./components/myOrders/MyOrders";
+import OrderDetails from "./components/myOrders/OrderDetails";
+import Dashboard from "./components/admin/Dashboard";
+import Users from "./components/admin/Users";
+import Orders from "./components/admin/Orders";
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -26,6 +30,8 @@ import "./styles/login.scss";
 import "./styles/profile.scss";
 import "./styles/myorder.scss";
 import "./styles/table.scss";
+import "./styles/orderDetails.scss";
+import "./styles/dashboard.scss";
 function App() {
   return (
     <Router>
@@ -40,6 +46,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/me" element={<Profile />} />
         <Route path="/myorders" element={<MyOrders />} />
+        <Route path="/order/:id" element={<OrderDetails />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="/admin/orders" element={<Orders />} />
       </Routes>
       <Footer />
     </Router>

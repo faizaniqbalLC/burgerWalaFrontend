f63543452379e03da52a799b335860a3e8f4dcbb
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-16 18:10:26 +0500

    worked on login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { FcGoogle } from "react-icons/fc";
+const Login = () => {
+  return (
+    <section className="login">
+      <motion.button initial={{ y: "-100vh" }} animate={{ y: 0 }}>
+        Login With Google <FcGoogle />
+      </motion.button>
+    </section>
+  );
+};
+
+export default Login;

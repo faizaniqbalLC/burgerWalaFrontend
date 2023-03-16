f06c46b10471c748cde82c2d9187243e3753ec6b
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-16 18:54:22 +0500

    app compo changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import Home from "./components/home/Home";
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
 import Contact from "./components/contact/Contact";
+import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
+import ConfirmOrder from "./components/cart/ConfirmOrder";
+import PaymentSuccess from "./components/cart/PaymentSuccess";
+import Login from "./components/login/Login";
+import Profile from "./components/profile/Profile";
+import MyOrders from "./components/myOrders/MyOrders.jsx";
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -11,13 +18,28 @@ import "./styles/founder.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
+import "./styles/cart.scss";
+import "./styles/shipping.scss";
+import "./styles/confirmorder.scss";
+import "./styles/paymentSuccess.scss";
+import "./styles/login.scss";
+import "./styles/profile.scss";
+import "./styles/myorder.scss";
+import "./styles/table.scss";
 function App() {
   return (
     <Router>
-      <Header />
+      <Header isAuthenticated={true} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/confirmOrder" element={<ConfirmOrder />} />
+        <Route path="/paymentSuccess" element={<PaymentSuccess />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/me" element={<Profile />} />
+        <Route path="/myorders" element={<MyOrders />} />
       </Routes>
       <Footer />
     </Router>

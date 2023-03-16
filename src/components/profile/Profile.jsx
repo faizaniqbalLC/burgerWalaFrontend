f96195d1ee06910cafc82c8b8eb4cf5853d18753
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-16 18:12:38 +0500

    worked on profile component

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { motion } from "framer-motion";
+import me from "../../assets/founder.png";
+import { Link } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+
+const Profile = () => {
+  const options = {
+    initial: {
+      y: "-100%",
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+  return (
+    <section className="profile">
+      <main>
+        <motion.img src={me} alt="User" {...options} />
+        <motion.h5 {...options} transition={{ delay: 0.3 }}>
+          Muhammad Fizan
+        </motion.h5>
+        <motion.div {...options} transition={{ delay: 0.5 }}>
+          <Link
+            to="/admin/dashboard"
+            style={{
+              borderRadius: 0,
+              backgroundColor: "rgb(40,40,40)",
+            }}>
+            <MdDashboard /> Dashboard
+          </Link>
+        </motion.div>
+        <motion.div
+          initial={{
+            x: "-100vw",
+            opacity: 0,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+          }}>
+          <Link to="/myorders">Orders</Link>
+        </motion.div>
+
+        <motion.button
+          initial={{
+            x: "-100vw",
+            opacity: 0,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+          }}
+          transition={{
+            delay: 0.3,
+          }}>
+          Logout
+        </motion.button>
+      </main>
+    </section>
+  );
+};
+
+export default Profile;

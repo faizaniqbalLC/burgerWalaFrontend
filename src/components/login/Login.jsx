9a42a73d3877f66bfae9ba6c07142d18537af43b
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-29 22:35:20 +0500

    worked on google login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { FcGoogle } from "react-icons/fc";
+import { server } from "../../redux/store";
 const Login = () => {
+  const loginHandler = () => {
+    window.open(`${server}/googlelogin`, "_self");
+  };
+
   return (
     <section className="login">
-      <motion.button initial={{ y: "-100vh" }} animate={{ y: 0 }}>
+      <motion.button
+        initial={{ y: "-100vh" }}
+        animate={{ y: 0 }}
+        onClick={loginHandler}>
         Login With Google <FcGoogle />
       </motion.button>
     </section>
